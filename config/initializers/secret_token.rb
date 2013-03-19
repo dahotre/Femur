58413a0600324b81e26844f709b3a53927266eb3
dahotre <dahotre.aniket@gmail.com>
2013-03-18 20:50:33 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Femur::Application.config.secret_token = '8b311b3da5c77c144b3054faee8e4e9f5625814393491d3def71db15fada3fe0d9eb86136a6bb02fbcd6c7b43819798866b3142481fcee43ef86b4b5935c1d40'
