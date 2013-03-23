af54a1fcdfeb08155dc1a0869e6c083cc8c2c951
Aniket Dahotre <dahotre.aniket@gmail.com>
2013-03-23 14:24:48 -0600

    Added google font
    google font added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,16 @@
 //= require handlebars.runtime
 //= require_tree ./templates
 //= require_tree .
+  WebFontConfig = {
+    google: { families: [ 'Muli:300:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
+  _.templateSettings.interpolate = /\{\{(.+?)\}\}/g;
